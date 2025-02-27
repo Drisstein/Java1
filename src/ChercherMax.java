7a95b3292ce029818f2c3aa5f75abe5852f523ee
drisstein <idrissahmad1903@gmail.com>
2025-02-27 23:55:37 +0100

    Un programme qui permet de renvoyer le max et le min !

---
+++ src/ChercherMax.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ChercherMax {
+    public static void Maxmin() {
+        int x;
+        int y;
+        System.out.println("Entrez un nombre : ");
+        int x = lire.nextInt();
+    }
+}
