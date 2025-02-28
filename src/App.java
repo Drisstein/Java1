71f7bb733f4c9fca16dad737f3df35861914bcb2
drisstein <idrissahmad1903@gmail.com>
2025-02-28 21:53:53 +0100

    WitchCase | Programme affichant le jour de la semaine

--- src/App.java
+++ src/App.java
@@ -1,13 +1,15 @@
 public class App {
     public static void main(String[] args) {
-        /*
-         * TrieDecroissant tD = new TrieDecroissant(); // Instanciation de
-         * classeTrieDecroissant
-         * tD.saisiDecroissant(); //Methode d instance
-         */
-        // EXERCICE CHERCHERMAX par une instance de classe
-        ChercherMax.maxMin();
-        TrieDecroissant.saisiDecroissant(); // Methode de classe
+        // /*
+        // * TrieDecroissant tD = new TrieDecroissant(); // Instanciation de
+        // * classeTrieDecroissant
+        // * tD.saisiDecroissant(); //Methode d instance
+        // */
+        // // EXERCICE CHERCHERMAX par une instance de classe
+        // ChercherMax.maxMin();
+        // TrieDecroissant.saisiDecroissant(); // Methode de classe
+        // Exercice 3 les jours de la semaine
+        SwitchCase.jourSemaine(args);
 
     }
 }
