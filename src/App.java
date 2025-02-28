48089d1e7b73c830cd659395a8338767702c0de1
drisstein <idrissahmad1903@gmail.com>
2025-02-28 14:04:48 +0100

    Un programme qui permet de renvoyer le max et le min ! [

--- src/App.java
+++ src/App.java
@@ -1,8 +1,13 @@
 public class App {
-    public static void main(String[] args)  {
-    // TrieDecroissant tD = new TrieDecroissant(); // Instanciation de classeTrieDecroissant 
-        // tD.saisiDecroissant(); //Methode d instance
+    public static void main(String[] args) {
+        /*
+         * TrieDecroissant tD = new TrieDecroissant(); // Instanciation de
+         * classeTrieDecroissant
+         * tD.saisiDecroissant(); //Methode d instance
+         */
 
-       TrieDecroissant.saisiDecroissant(); // Methode de classe
+        TrieDecroissant.saisiDecroissant(); // Methode de classe
+        // EXERCICE CHERCHERMAX par une instance de classe
+        ChercherMax.maxMin();
     }
 }
--- src/ChercherMax.java
+++ src/ChercherMax.java
@@ -1,10 +1,30 @@
 import java.util.Scanner;
 
 public class ChercherMax {
-    public static void Maxmin() {
+    public static void maxMin() {
+        try (Scanner lire = new Scanner(System.in)) {
         int x;
         int y;
-        System.out.println("Entrez un nombre : ");
-        int x = lire.nextInt();
+        while (true) {
+            System.out.println("Entrez un nombre : ");
+            x = lire.nextInt();
+            System.out.println("Entrez un autre nombre : ");
+            y = lire.nextInt();
+
+            if (x > y) {
+                System.out.println("Le plus grand est : " + x);
+                System.out.println("Le plus petit est : " + y);
+            } else {
+                System.out.println("Le plus grand est : " + y);
+                System.out.println("Le plus petit est : " + x);
+            }
+            System.out.println("Voulez-vous continuer ? (O/N)");
+            String reponse = lire.next();
+            if (reponse.equals("N")) {
+                break;
+            }
+        }
+
     }
+
 }
