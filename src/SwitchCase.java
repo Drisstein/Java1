47d3f2bf0b1799c3b4f118c72d2de79a5fde3f8d
drisstein <idrissahmad1903@gmail.com>
2025-03-01 01:38:31 +0100

    Ajout d'une calculatrice !

--- src/SwitchCase.java
+++ src/SwitchCase.java
@@ -4,6 +4,7 @@ public class SwitchCase {
     public static void jourSemaine(String[] args) {
         Scanner lire = new Scanner(System.in);
         while (true) {
+            // Programme 1: Jours de la semaine
             System.out.print("Veuillez saisir le jour : ");
             int x = lire.nextInt();
             switch (x) {
@@ -30,6 +31,33 @@ public class SwitchCase {
                     break;
                 default:
                     System.out.println("Veuillez saisir un nombre entre 1 et 7");
+            }
+            // Programme 2 : Calculatrice
+            System.out.println("Veuillez saisir deux nombres : ");
+            int a = lire.nextInt();
+            int b = lire.nextInt();
+            System.out.print("Nature de l'operation : ");
+            char op = lire.next().charAt(0);
+            switch (op) {
+                case '+':
+                    System.out.println(a + " + " + b + " = " + (a + b));
+                    break;
+                case '-':
+                    System.out.println(a + " - " + b + " = " + (a - b));
+                    break;
+                case '*':
+                    System.out.println(a + " * " + b + " = " + (a * b));
+                    break;
+                case '%':
+                    System.out.println(a + " % " + b + " = " + (a % b));
+                    break;
+                case '/':
+                    System.out.println(a + " / " + b + " = " + (a / b));
+                    break;
+
+                default:
+                    System.out.println("Operation invalide ! Veuillez reessayer svp ! ");
+                    break;
             }
         }
 
