e49f2008fce573b5bff3933bec2ae1d88b4eadf0
drisstein <idrissahmad1903@gmail.com>
2025-03-03 16:19:49 +0100

    Calculatrice de Factorielle !

--- src/Factorielle.java
+++ src/Factorielle.java
@@ -13,8 +13,13 @@ public class Factorielle {
     public static void main(String[] args) {
         Scanner lire = new Scanner(System.in);
         System.out.println("Calculatrice de Factorielle !");
-        System.out.println("Veuillez saisir un nombre!");
-        int x = lire.nextInt();
-        System.out.println(fact(x));
+        while (true) {
+            int x;
+            do {
+                System.out.print("Veuillez saisir un nombre :");
+                x = lire.nextInt();
+            } while (x < 0);
+            System.out.println("Le factorielle de " + x + " est : " + fact(x));
+        }
     }
 }
