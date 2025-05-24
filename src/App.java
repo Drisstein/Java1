382bf71c9d19a11570fc1df8df64d24621d58902
drisstein <idrissahmad1903@gmail.com>
2025-05-24 18:20:19 +0000

    Exercices du TD1 2025

--- src/App.java
+++ src/App.java
@@ -1,57 +1,57 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class App {
-    public static void main(String[] args) {
-        // /*
-        /* L'ENSEMBLE DE CES EXERCICES SONT ITERES PAR WHILE (...) */
-        // * TrieDecroissant tD = new TrieDecroissant(); // Instanciation de
-        // * classeTrieDecroissant
-        // * tD.saisiDecroissant(); //Methode d instance
-        // */
-        // 1
-        // // EXERCICE CHERCHERMAX par une iénstance de classe
-        // ChercherMax.maxMin();
+// public class App {
+// public static void main(String[] args) {
+// // /*
+// /* L'ENSEMBLE DE CES EXERCICES SONT ITERES PAR WHILE (...) */
+// // * TrieDecroissant tD = new TrieDecroissant(); // Instanciation de
+// // * classeTrieDecroissant
+// // * tD.saisiDecroissant(); //Methode d instance
+// // */
+// // 1
+// // // EXERCICE CHERCHERMAX par une iénstance de classe
+// // ChercherMax.maxMin();
 
-        // 2
-        // TrieDecroissant.saisiDecroissant(); // Methode de classe
+// // 2
+// // TrieDecroissant.saisiDecroissant(); // Methode de classe
 
-        // 3
-        // Exercice 3 les jours de la semaine
-        // SwitchCase.jourSemaine(args);
+// // 3
+// // Exercice 3 les jours de la semaine
+// // SwitchCase.jourSemaine(args);
 
-        // 4
-        // Exercice 4 PariteNombre
-        // PariteNombre.paritePositif(args);
+// // 4
+// // Exercice 4 PariteNombre
+// // PariteNombre.paritePositif(args);
 
-        // 8 TD1 2024
-        // Exo 8 : Factorielle.fact();
+// // 8 TD1 2024
+// // Exo 8 : Factorielle.fact();
 
-        // 9
-        // Exo 9 : Table de Multiplication
-        // TableMultip.table(args);
+// // 9
+// // Exo 9 : Table de Multiplication
+// // TableMultip.table(args);
 
-        // 10
-        // Point.main();
-        // Point p1 = new Point(3, 4);
-        // Point p2 = new Point(0, 0);
+// // 10
+// // Point.main();
+// // Point p1 = new Point(3, 4);
+// // Point p2 = new Point(0, 0);
 
-        // Scanner sc = new Scanner(System.in);
-        // System.out.println("Entrez les coordonnées du point p2 : ");
-        // int a = sc.nextInt();
-        // int b = sc.nextInt();
+// // Scanner sc = new Scanner(System.in);
+// // System.out.println("Entrez les coordonnées du point p2 : ");
+// // int a = sc.nextInt();
+// // int b = sc.nextInt();
 
-        // p2.setX(a);
-        // p2.setY(b);
+// // p2.setX(a);
+// // p2.setY(b);
 
-        // // p2 = new Point(a, b);
-        // System.out.println("Coordonnées initiales de p2 : " + p2.getX() + ", " +
-        // p2.getY());
+// // // p2 = new Point(a, b);
+// // System.out.println("Coordonnées initiales de p2 : " + p2.getX() + ", " +
+// // p2.getY());
 
-        // new Point();
+// // new Point();
 
-        // 11 : Correction by GPT
-        // CorrTabMinmax.main(args);
-        SaisiTableauCorrection.main(args);
+// // 11 : Correction by GPT
+// // CorrTabMinmax.main(args);
+// SaisiTableauCorrection.main(args);
 
-    }
-}
+// }
+// }
